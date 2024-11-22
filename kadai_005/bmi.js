b97f6e2121298208c5692d857eea64f891bdd53a
Kaito-enginner <itatani.kaito-saber@outlook.jp>
2024-11-23 00:06:46 +0900

    「四則演算をしよう」kadai_005を再アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,8 +1,12 @@
 // 体重
-let weight = 68;
+let Weight = 68;
 
 // 身長
-let height = 1.7;
+let Height = 1.7;
 
-// BMI（体格指数）出力
-console.log (height*height/weight);
+// BMI（体格指数）
+let Height2 = Height*Height;
+let BMI = Weight/Height2
+
+// BMI（体格指数）を出力
+console.log(BMI);
